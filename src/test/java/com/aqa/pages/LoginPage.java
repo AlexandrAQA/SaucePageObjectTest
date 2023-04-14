17ca860937cf92d574772042334e38e89319cc50
Alex Panaev <panaev.as@gmail.com>
2023-04-14 22:20:35 +0400

    added LoginPage with private vars(locators)

---
+++ src/test/java/com/aqa/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.aqa.pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+
+   private By userNameInput = By.id("user-name");
+   private By passwordInput  = By.id("password");
+   private By loginButton  = By.id("login-button");
+
+
+}
