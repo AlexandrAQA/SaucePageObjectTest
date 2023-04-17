ebbf0fa0b284c07b389c2e14d3a807dd2b023ed4
Alex Panaev <panaev.as@gmail.com>
2023-04-17 18:16:09 +0400

    added ProductsPage vars, constructor, getTitle()

--- src/test/java/com/aqa/pages/ProductsPage.java
+++ src/test/java/com/aqa/pages/ProductsPage.java
@@ -1,8 +1,19 @@
 package com.aqa.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProductsPage {
     //By - this is all locators type
    private final By TITLE = By.cssSelector(".title");
+   WebDriver driver;
+
+    public ProductsPage( WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getTitle(){
+       return driver.findElement(TITLE);
+   }
 }
