4245c20b79c83994636a3feb4c35ad8e9a7054e2
Alex Panaev <panaev.as@gmail.com>
2023-04-21 09:54:30 +0400

    added CartPage with constructor

---
+++ src/test/java/com/aqa/pages/CartPage.java
@@ -0,0 +1,10 @@
+package com.aqa.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends BasePage{
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+}
