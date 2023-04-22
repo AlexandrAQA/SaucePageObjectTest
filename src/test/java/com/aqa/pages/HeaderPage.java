6098e79205c8be5c6ce18051fbe7c72a17f03d3a
Alex Panaev <panaev.as@gmail.com>
2023-04-22 12:40:33 +0400

    added HeaderPage with constructor

---
+++ src/test/java/com/aqa/pages/HeaderPage.java
@@ -0,0 +1,9 @@
+package com.aqa.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HeaderPage extends BasePage{
+    public HeaderPage(WebDriver driver) {
+        super(driver);
+    }
+}
