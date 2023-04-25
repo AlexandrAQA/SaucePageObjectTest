77328c7d6eaff6993e1a64a39e7c2d054035be8e
Alex Panaev <panaev.as@gmail.com>
2023-04-25 13:56:46 +0400

    updated ProductsTest

--- src/test/java/com/aqa/tests/ProductsTest.java
+++ src/test/java/com/aqa/tests/ProductsTest.java
@@ -6,10 +6,14 @@ public class ProductsTest extends BaseTest {
 
     @Test
     public void productsShouldBeAddedToCart() {
+        final String firstSauceLabsFleeceJacket = "Sauce Labs Fleece Jacket";
+        final String secondSauceLabsBackpack = "Sauce Labs Backpack";
+
         loginPage.open();
         loginPage.loginAsValidUser();
-        productsPage.addToCart("Sauce Labs Fleece Jacket");
+        productsPage.addToCart(firstSauceLabsFleeceJacket);
+        productsPage.addToCart(secondSauceLabsBackpack);
         headerPage.openCart();
-
+        cartPage.removeProductFromCart(firstSauceLabsFleeceJacket);
     }
 }
