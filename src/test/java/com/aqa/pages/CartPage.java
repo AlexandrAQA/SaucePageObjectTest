6bcf13549a1c8b877d991f27b626cfcfc29eeff4
Alex Panaev <panaev.as@gmail.com>
2023-04-25 14:25:12 +0400

    updated CartPage with var & getAllProductsInCart()

--- src/test/java/com/aqa/pages/CartPage.java
+++ src/test/java/com/aqa/pages/CartPage.java
@@ -2,11 +2,15 @@ package com.aqa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class CartPage extends BasePage{
 
     //Sauce Labs Backpack
     String removeButton = "//div[text()='%s']//ancestor::div[@class='cart_item']//button";
+    By productItemInCart = By.xpath("//div[@class='inventory_item_name']");
 
     public CartPage(WebDriver driver) {
         super(driver);
@@ -16,5 +20,9 @@ public class CartPage extends BasePage{
         By fullLocator = By.xpath(String.format(removeButton, productName));
         //find the element and click
         driver.findElement(fullLocator).click();
+    }
+
+    public List<WebElement> getAllProductsInCart(){
+        return driver.findElements(productItemInCart);
     }
 }
