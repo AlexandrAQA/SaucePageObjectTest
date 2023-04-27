747d5a0eeef2977e28d85945eb783934c7b6c7bd
Alex Panaev <panaev.as@gmail.com>
2023-04-27 21:14:35 +0400

    updated LoginPage with @CacheLookup annotation

--- src/test/java/com/aqa/pages/LoginPage.java
+++ src/test/java/com/aqa/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.aqa.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
@@ -9,11 +10,17 @@ import org.testng.annotations.Test;
 public class LoginPage extends BasePage {
 
     @FindBy(id = "user-name")
+    @CacheLookup
     private WebElement userNameInput;
+
     @FindBy(id = "password")
+    @CacheLookup
     private WebElement passwordInput;
+
     @FindBy(id = "login-button")
+    @CacheLookup
     private WebElement loginButton;
+
     @FindBy(css = "h3[data-test='error']")
     private WebElement error;
 
