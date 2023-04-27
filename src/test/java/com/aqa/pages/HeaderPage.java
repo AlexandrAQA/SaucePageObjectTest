08fa1da1342c08c461cb5f56f41e66450a13ae05
Alex Panaev <panaev.as@gmail.com>
2023-04-27 21:19:09 +0400

    updated HeaderPage with @FindBy & @CacheLookup annotations

--- src/test/java/com/aqa/pages/HeaderPage.java
+++ src/test/java/com/aqa/pages/HeaderPage.java
@@ -2,16 +2,24 @@ package com.aqa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HeaderPage extends BasePage{
 
-    private By CART_LINK = By.xpath("//div[@id='shopping_cart_container']/a");
+    @FindBy(xpath = "//div[@id='shopping_cart_container']/a")
+    @CacheLookup
+    private WebElement cartLink;
+
 
     public HeaderPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver,this);
     }
 
     public void openCart(){
-        driver.findElement(CART_LINK).click();
+        cartLink.click();
     }
 }
