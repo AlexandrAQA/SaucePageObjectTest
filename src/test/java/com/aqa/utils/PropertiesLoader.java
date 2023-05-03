034cafef810063b3ab916980a5243c0c4b52d8da
Alex Panaev <panaev.as@gmail.com>
2023-05-03 18:39:13 +0400

    added PropertiesLoader with empty method

---
+++ src/test/java/com/aqa/utils/PropertiesLoader.java
@@ -0,0 +1,13 @@
+package com.aqa.utils;
+
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    public static Properties loadProperties(){
+        Properties properties = new Properties();
+
+
+        return properties;
+    }
+}
