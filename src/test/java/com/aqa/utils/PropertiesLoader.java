03e4b2d337f50d82cb079266bc749f73032fdfc7
Alex Panaev <panaev.as@gmail.com>
2023-05-04 19:56:42 +0400

    added 2 Methods into PropertiesLoader

--- src/test/java/com/aqa/utils/PropertiesLoader.java
+++ src/test/java/com/aqa/utils/PropertiesLoader.java
@@ -1,13 +1,28 @@
 package com.aqa.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesLoader {
 
-    public static Properties loadProperties(){
+    public static Properties loadProperties(String fileName) {
         Properties properties = new Properties();
-
-
+        try (InputStream inputStream = PropertiesLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            properties.load(inputStream);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
         return properties;
     }
+
+    public static Properties loadProperties(){
+        Properties properties = loadProperties("config.properties");
+        String user = properties.getProperty("user");
+        Properties userProperties = loadProperties(user + ".properties");
+        properties.putAll(userProperties);
+        return properties;
+    }
+
+
 }
