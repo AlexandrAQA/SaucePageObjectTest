d707653587f1bb28298cdb4bc04edcf3f424125b
Alex Panaev <panaev.as@gmail.com>
2023-05-09 19:43:06 +0400

    updated HeaderPage and added empty LogoutTest

---
+++ src/test/java/com/aqa/tests/LogoutTest.java
@@ -0,0 +1,14 @@
+package com.aqa.tests;
+
+import org.testng.annotations.Test;
+
+public class LogoutTest extends BaseTest{
+
+    @Test(enabled = true)
+    public void logoutInBurgerMenuShouldWork(){
+//        LoginTest loginTest = new LoginTest();
+//        loginTest.userShouldLoginWithValidCredentials();
+
+    }
+
+}
