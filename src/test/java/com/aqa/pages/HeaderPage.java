e56184d1e012e9aabee58a9aa3c627764d4f7bad
Alex Panaev <panaev.as@gmail.com>
2023-05-09 19:42:05 +0400

    updated HeaderPage

--- src/test/java/com/aqa/pages/HeaderPage.java
+++ src/test/java/com/aqa/pages/HeaderPage.java
@@ -3,23 +3,41 @@ package com.aqa.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HeaderPage extends BasePage{
 
+    private final static String BURGER_MENU_LOCATOR = "//nav[@class='bm-item-list']";
+
     @FindBy(xpath = "//div[@id='shopping_cart_container']/a")
-    @CacheLookup
     private WebElement cartLink;
 
+    @FindBy(xpath = BURGER_MENU_LOCATOR)
+    private WebElement burgerMenu;
+
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement burgerMenuButton;
+
+    @FindBy(xpath = "//div[@class='bm-menu']//a[text()='Logout']")
+    private WebElement burgerMenuLogoutItem;
 
     public HeaderPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
-    public void openCart(){
+    public void openCart() {
         cartLink.click();
+    }
+
+    public void openBurgerMenu() {
+        burgerMenuButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(BURGER_MENU_LOCATOR)));
+    }
+
+    public void logout() {
+        burgerMenuLogoutItem.click();
     }
 }
