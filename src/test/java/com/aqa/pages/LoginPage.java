f0bb3eab5cc8d2c9eea32f810b4829d08feb299d
Alex Panaev <panaev.as@gmail.com>
2023-05-10 15:13:55 +0400

    added CheckoutOverviewPage

--- src/test/java/com/aqa/pages/LoginPage.java
+++ src/test/java/com/aqa/pages/LoginPage.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.Properties;
 
 public class LoginPage extends BasePage {
