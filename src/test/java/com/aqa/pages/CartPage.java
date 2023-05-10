704624f3d5e55130aeb9e015d0cbc45212f6f729
Alex Panaev <panaev.as@gmail.com>
2023-05-10 16:37:37 +0400

    updated CartPage

--- src/test/java/com/aqa/pages/CartPage.java
+++ src/test/java/com/aqa/pages/CartPage.java
@@ -10,25 +10,51 @@ import java.util.List;
 
 public class CartPage extends BasePage{
 
-    //Sauce Labs Backpack
-    String removeButton = "//div[text()='%s']//ancestor::div[@class='cart_item']//button";
+    @FindBy(css = ".title")
+    private WebElement title;
+    @FindBy(id = "checkout")
+    private WebElement checkout;
+    @FindBy(id = "continue-shopping")
+    private WebElement continueShoppingButton;
     @FindBy(xpath = "//div[@class='inventory_item_name']")
-    private List<WebElement> productItemInCart;
+    private List<WebElement> allProductsInCart;
 
-    //By productItemInCart = By.xpath("//div[@class='inventory_item_name']");
+
+    private final String productInCart = "//div[text()='%s']//ancestor::div[@class='cart_item']";
+    private final String removeButton = "//div[text()='%s']//ancestor::div[@class='cart_item']//button[text()='Remove']";
+    private final String product = "//div[text()='%s']//ancestor::div[@class='cart_item']";
 
     public CartPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {
+        driver.get(baseUrl + "cart.html");
+    }
+
+    public List<WebElement> getAllProductsInCart() {
+        return allProductsInCart;
+    }
+
+    public void checkout() {
+        checkout.click();
     }
 
     public void removeProductFromCart(String productName) {
-        By fullLocator = By.xpath(String.format(removeButton, productName));
-        //find the element and click
-        driver.findElement(fullLocator).click();
+        driver.findElement(By.xpath(String.format(removeButton, productName))).click();
     }
 
-    public List<WebElement> getAllProductsInCart(){
-        return productItemInCart;
+    public WebElement getProduct(String productName) {
+        By fullLocator = By.xpath(String.format(product, productName));
+        return driver.findElement(fullLocator);
+    }
+
+    public WebElement getTitle() {
+        return title;
+    }
+
+    public void continueShopping() {
+        continueShoppingButton.click();
     }
 }
