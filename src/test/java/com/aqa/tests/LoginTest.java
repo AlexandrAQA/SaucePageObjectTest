0ce8a5724ddef91f969f8c70b73da3d2e6f54d20
Alex Panaev <panaev.as@gmail.com>
2023-05-11 12:51:01 +0400

    updated LoginTest

--- src/test/java/com/aqa/tests/LoginTest.java
+++ src/test/java/com/aqa/tests/LoginTest.java
@@ -1,40 +1,54 @@
 package com.aqa.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import com.aqa.utils.PropertiesLoader;
 import org.testng.annotations.Test;
+
+import java.util.Properties;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void userShouldLoginWithValidCredentials() {
-        loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-        WebElement titleProducts = driver.findElement(By.cssSelector(".title"));
-        Assert.assertTrue(productsPage.getTitle().isDisplayed(), "User was not logged in");
-    }
-
-    @Test
-    public void passwordShouldBeCorrectForLogin() {
-        loginPage.open();
-        loginPage.login("standard_user", " ");
-        Assert.assertEquals(loginPage.getErrorFromThePage(),
-                errorTextWhenPswIncorrect, "the error message is incorrect");
+        loginSteps.loginAsStandardUser();
+        assertTrue(productsPage.getTitle().isDisplayed(), "User was not logged in");
     }
 
     @Test
     public void passwordShouldBeRequiredForLogin() {
-        loginPage.open();
-        loginPage.login("standard_user", "");
-        Assert.assertEquals(loginPage.getErrorFromThePage(),
-                errorTextWhenPswIsEmpty, "the error message is incorrect");
+        loginSteps.login("standard_user", "");
+        String expected = "Epic sadface: Password is required";
+        assertEquals(loginPage.getError(), expected, "The error is incorrect");
     }
+
     @Test
-    public void loginShouldBeRequiredForLogin() {
-        loginPage.open();
-        loginPage.login("", "");
-        Assert.assertEquals(loginPage.getErrorFromThePage(),
-                errorTextWhenLoginIsEmpty, "the error message is incorrect");
+    public void userNameShouldBeRequiredForLogin() {
+        loginSteps.login("", "12345");
+        String expected = "Epic sadface: Username is required";
+        assertEquals(loginPage.getError(), expected, "The error is incorrect");
+    }
+
+    @Test
+    public void userShouldNotBeLoggedInBeWithInvalidPassword() {
+        loginSteps.login("standard_user", "12345");
+        String expected = "Epic sadface: Username and password do not match any user in this service";
+        assertEquals(loginPage.getError(), expected, "The error is incorrect");
+    }
+
+    @Test
+    public void userShouldNotBeLoggedInBeWithInvalidUserName() {
+        loginSteps.login("standard_user1", "secret_sauce");
+        String expected = "Epic sadface: Username and password do not match any user in this service";
+        assertEquals(loginPage.getError(), expected, "The error is incorrect");
+    }
+
+    @Test
+    public void lockedOutUserShouldNotBeLoggedIn() {
+        Properties properties = PropertiesLoader.loadProperties("locked_out_user.properties");
+        loginSteps.login(properties.getProperty("username"), properties.getProperty("password"));
+        String expected = "Epic sadface: Sorry, this user has been locked out.";
+        assertEquals(loginPage.getError(), expected, "The error is incorrect");
     }
 }
