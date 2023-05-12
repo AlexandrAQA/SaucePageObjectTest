6c27048fd500e09a01787eaa8fadefd476fc3e34
Alex Panaev <panaev.as@gmail.com>
2023-05-12 12:55:47 +0400

    updated ProductsTest

--- src/test/java/com/aqa/tests/ProductsTest.java
+++ src/test/java/com/aqa/tests/ProductsTest.java
@@ -7,25 +7,37 @@ import java.util.List;
 
 public class ProductsTest extends BaseTest {
 
+    public static final String SAUCE_LABS_BACKPACK = "Sauce Labs Backpack";
+    public static final String SAUCE_LABS_BOLT_T_SHIRT = "Sauce Labs Bolt T-Shirt";
+
     @Test
-    public void productsShouldBeAddedToCart() {
-        String firstSauceLabsFleeceJacket = "Sauce Labs Fleece Jacket";
-        String secondSauceLabsBackpack = "Sauce Labs Backpack";
-
-        loginPage.open();
-        loginPage.loginAsValidUser();
-        productsPage.addToCart(firstSauceLabsFleeceJacket);
-        productsPage.addToCart(secondSauceLabsBackpack);
+    public void productShouldBeAddedToCart() {
+        loginSteps.loginAsDefaultUser();
+        productsPage.addToCart(SAUCE_LABS_BACKPACK);
         headerPage.openCart();
-        cartPage.removeProductFromCart(secondSauceLabsBackpack);
-        List<WebElement> allProductsInCartAfterRemove = cartPage.getAllProductsInCart();
-        //we can use CollectionUtils.size(allProductsInCartAfterRemove) by org.apache.commons.collections4.CollectionUtils;
-        // instead of Assert.assertEquals(allProductsInCartAfterRemove.size()
+        List<WebElement> allProductsInCart = cartPage.getAllProductsInCart();
 
-        Assert.assertEquals(allProductsInCartAfterRemove.size(),
-                1, "There is only 1 product in the cart after removing");
-        Assert.assertEquals(allProductsInCartAfterRemove.get(0).getText(),
-                firstSauceLabsFleeceJacket, "\"Sauce Labs Fleece Jacket\"" +
-                        " is in the cart after removing another one");
+        Assert.assertEquals(allProductsInCart.size(), 1,
+                "One product should be in the cart");
+        Assert.assertEquals(allProductsInCart.get(0).getText(), SAUCE_LABS_BACKPACK,
+                "\"" + SAUCE_LABS_BACKPACK + "\" product should be in the cart");
+    }
+
+    @Test
+    public void productShouldBeRemovedFromCart() {
+        loginSteps.loginAsDefaultUser();
+        productsPage.addToCart(SAUCE_LABS_BACKPACK, SAUCE_LABS_BOLT_T_SHIRT);
+        headerPage.openCart();
+        List<WebElement> allProductsInCartBeforeRemove = cartPage.getAllProductsInCart();
+        Assert.assertEquals(allProductsInCartBeforeRemove.size(), 2,
+                "Two products should be in the cart");
+
+        cartPage.removeProductFromCart(SAUCE_LABS_BOLT_T_SHIRT);
+        List<WebElement> allProductsInCartAfterRemove = cartPage.getAllProductsInCart();
+
+        Assert.assertEquals(allProductsInCartAfterRemove.size(), 1,
+                "Only one product should left in the cart after removing");
+        Assert.assertEquals(allProductsInCartAfterRemove.get(0).getText(), SAUCE_LABS_BACKPACK,
+                "\"" + SAUCE_LABS_BACKPACK + "\" product should be in the cart after removing");
     }
 }
