8efcd067dfd6509314aed3a0656b7dfc4503e426
Alex Panaev <panaev.as@gmail.com>
2023-05-31 10:20:34 +0400

    execute tests into 2 browsers 2nd part

--- src/test/java/com/aqa/tests/BaseTest.java
+++ src/test/java/com/aqa/tests/BaseTest.java
@@ -6,9 +6,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -23,25 +25,34 @@ public class BaseTest {
     CheckoutOverviewPage checkoutOverviewPage;
     LoginSteps loginSteps;
 
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() {
-        //Initialize web driver and create driver instance
-        WebDriverManager.chromedriver().setup();
-        //created ChromeOptions
-        ChromeOptions options = new ChromeOptions();
-        //without launching web browser!!!
-        options.addArguments("--headless=new");
+    public void setUp(String browser) {
+        if (browser.equals("chrome")) {
+            //Initialize web driver and create driver instance
+            WebDriverManager.chromedriver().setup();
+            //created ChromeOptions
+            ChromeOptions options = new ChromeOptions();
+            //without launching web browser!!!
+            //options.addArguments("--headless=new");
 
-        driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //Set up driver settings
-        driver.manage().window().maximize();
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            //Set up driver settings
+            driver.manage().window().maximize();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
         //Pages
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         headerPage = new HeaderPage(driver);
         cartPage = new CartPage(driver);
+
         //Steps
         loginSteps = new LoginSteps(driver);
         checkoutPage = new CheckoutPage(driver);
