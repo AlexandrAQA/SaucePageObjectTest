68c3ea7c4d00b55b4f3e26c0c502619971402fc7
Alex Panaev <panaev.as@gmail.com>
2023-06-06 11:58:41 +0400

    iRetryAnalyzer class + method

--- src/test/java/com/aqa/utils/retry/RetryAnalyzer.java
+++ src/test/java/com/aqa/utils/retry/RetryAnalyzer.java
@@ -1,4 +1,26 @@
 package com.aqa.utils.retry;
 
-public class RetryAnalyzer {
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+    private int count = 0;
+    private static final int MAX_RETRY = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (!iTestResult.isSuccess()) {
+            if (count < MAX_RETRY) {
+                count++;
+                iTestResult.setStatus(ITestResult.FAILURE);
+                System.out.println("Retrying once again");
+                return true;
+            } else {
+                iTestResult.setStatus(ITestResult.FAILURE);
+            }
+        } else {
+            iTestResult.setStatus(ITestResult.SUCCESS);
+        }
+        return false;
+    }
 }
